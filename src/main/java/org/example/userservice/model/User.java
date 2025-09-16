bbefd1dca7e060e96279128531506e18328b6bcd
Shejuti <shejuti.feroz@tigerit.com>
2025-09-16 15:37:50 +0600

    Users can register

--- src/main/java/org/example/userservice/model/User.java
+++ src/main/java/org/example/userservice/model/User.java
@@ -1,12 +1,11 @@
 package org.example.userservice.model;
 
-import jakarta.persistence.Table;
-
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,13 +19,46 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    // No-args constructor required by Hibernate
     public User() {}
 
+    // Constructor for convenience
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    // Getters and setters here (can generate via IntelliJ)
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
